68dbbfc3e57bc5555b4c29aa94c806a0cd67277b
Diego <Diego@MacBook-Air-de-Diego.local>
2018-09-22 21:19:24 -0500

    se limpian los archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,7 @@ import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
+
 
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 import { RegisterComponent } from './register/register.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 
 
 const appRoutes: Routes = [
@@ -16,10 +13,5 @@ const appRoutes: Routes = [
     { path: '**', component: PagenotfoundComponent}
 ];
 
-/*@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})
-export class AppRoutingModule {}*/
 
 export const APP_ROUTS = RouterModule.forRoot( appRoutes, { useHash: true} );
