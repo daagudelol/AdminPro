073c0d8f90c0ed50ccd3dc499fd2df969024ae17
Diego <Diego@MacBook-Air-de-Diego.local>
2018-09-22 23:21:57 -0500

    se terminan las rutas

--- src/app/pages/pages.routs.ts
+++ src/app/pages/pages.routs.ts
@@ -1,9 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { PagesComponent } from '../pages.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { ProgressComponent } from './progress.component';
-import { Graficas1Component } from '../graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
 
 
 const pagesRoutes: Routes = [
